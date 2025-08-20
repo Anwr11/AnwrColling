d986513937744745db71df31d9b3f83df8cdfa34
Anwr11 <84395464+Anwr11@users.noreply.github.com>
2025-08-20 21:08:42 +0300

    wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,6 @@
 from app import create_app, socketio
-app = create_app()
-# يُستخدم مع: gunicorn wsgi:app -k eventlet -w 1 -b 0.0.0.0:$PORT
+app = create_app()   # مهم يكون فيه كائن اسمه app
+
+# ملاحظة: عند التشغيل على Render نستخدم gunicorn، أما هذا الكود يفيدك محلياً فقط:
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
