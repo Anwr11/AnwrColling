6036e5e215b899e1eeb1962483b18e3cfd7ce03c
Anwr11 <84395464+Anwr11@users.noreply.github.com>
2025-08-20 23:02:27 -0700

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,6 +0,0 @@
-from app import create_app, socketio
-app = create_app()   # مهم يكون فيه كائن اسمه app
-
-# ملاحظة: عند التشغيل على Render نستخدم gunicorn، أما هذا الكود يفيدك محلياً فقط:
-if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
