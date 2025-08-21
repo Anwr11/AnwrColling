7752beb0051de44dbb8927327b064303303e69b9
Anwr11 <84395464+Anwr11@users.noreply.github.com>
2025-08-21 15:28:55 +0300

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,3 @@
-from app import app
-
-if __name__ == "__main__":
-    app.run()
+from app import create_app, socketio
+app = create_app()
+# يُستخدم مع: gunicorn wsgi:app -k eventlet -w 1 -b 0.0.0.0:$PORT
